2dd6d6b6f6487a4febdd8408ac78b2dcbd050ef9
Amartyanil Chattopadhyay <61569496+amartyanil@users.noreply.github.com>
2023-10-01 23:22:19 +0530

    Update Encoder.c
    encoder corrected more

--- Encoder.c
+++ Encoder.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//read the file from fpIn and return the encoded string
-char* encoder(FILE* fpIn, int no_of_characters)
+//make a string the version of the encoded version of the plaintext file
+void encoder(FILE* plaintext, char* cipher_string, int no_of_characters)
 {
-    char* ciphertext = (char*) malloc((no_of_characters * 2 + 1) * sizeof(char));
-
     //read key length
     int key_length;
     printf("Enter key length: ");
     scanf("%d", &key_length);
+    while (getchar() != '\n');
 
     //read key
-    unsigned char ch;
+    char ch;
     int i; 
-    unsigned char* key = (unsigned char*) malloc(key_length * sizeof(unsigned char));
+    char* key = (char*) malloc((key_length) * sizeof(char));
     printf("Enter key: ");
-    fgets((char*)key, key_length + 1, stdin);
+    fgets(key, key_length + 1, stdin);
+    while (getchar() != '\n');
 
-    //store the encrypted key in ciphertext in hex-encoded format
     i = 0;
-    while (fscanf(fpIn, "%c", &ch) == 1)
+    
+    while ((ch = getc(plaintext)) != EOF)
     {
         if (ch != '\n')
         {
-            sprintf(ciphertext + 2 * i, "%02X", ch ^ key[i % key_length]);
+            cipher_string[i] = (char) ch ^ key[i % key_length];
             i++;
         }
-    }
-
+        
+    }    
+    cipher_string[i] = '\0';
+    free(key);
     //end the string
-    ciphertext[i] = '\0';
-
-    fclose(fpIn);
-
-    return ciphertext;
 }
