5473a82eb78d2f1e694d780e462912cbbc44e052
Amartyanil Chattopadhyay <61569496+amartyanil@users.noreply.github.com>
2023-10-02 20:26:02 +0530

    Update Encoder.c

--- Encoder.c
+++ Encoder.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 //make a string the version of the encoded version of the plaintext file
-void encoder(FILE* plaintext, char* cipher_string, int no_of_characters)
+void encoder(FILE* plaintext, char* cipher_string)
 {
     //read key length
     int key_length;
@@ -10,26 +10,24 @@ void encoder(FILE* plaintext, char* cipher_string, int no_of_characters)
     scanf("%d", &key_length);
     while (getchar() != '\n');
 
-    //read key
     char ch;
     int i; 
+    
     char* key = (char*) malloc((key_length) * sizeof(char));
     printf("Enter key: ");
     fgets(key, key_length + 1, stdin);
     while (getchar() != '\n');
 
     i = 0;
-    
-    while ((ch = getc(plaintext)) != EOF)
+
+    while ((ch = fgetc(plaintext)) != EOF)
     {
         if (ch != '\n')
         {
-            cipher_string[i] = (char) ch ^ key[i % key_length];
+            cipher_string[i] = (char) (ch ^ key[i % key_length]);
             i++;
-        }
-        
+        } 
     }    
     cipher_string[i] = '\0';
     free(key);
-    //end the string
 }
