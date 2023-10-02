037bb25bba75d3ecdba17625523a9cd2227f8f51
Amartyanil Chattopadhyay <61569496+amartyanil@users.noreply.github.com>
2023-10-02 20:24:53 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE* plain_text_input(char* filename, int no_of_characters);
-char* encoder(FILE* fpIn, int no_of_characters);
-//gcc -o main Main.c PlaintextInput.c Encoder.c
+void plain_text_input(FILE* filename, int no_of_characters);
+void encoder(FILE* plaintext, char* cipher_string);
+
 int main()
 {
-    int no_of_characters;
+    int no_of_characters;   
     printf("Enter number of characters in the plaintext: ");
     scanf("%d", &no_of_characters);
-    FILE* plaintext = fopen("plaintext.txt", "w");
-    plaintext = plain_text_input("plaintext.txt", no_of_characters);
+    
+    FILE* plaintext_w = fopen("plaintext.txt", "w");
+    while (getchar() != '\n');
+    plain_text_input(plaintext_w, no_of_characters);
+    fclose(plaintext_w);
 
-    char* cipher_string = (char*) malloc((no_of_characters * 2 + 1) * sizeof(char));
-    cipher_string = encoder(plaintext, no_of_characters);
-
+    FILE* plaintext_r = fopen("plaintext.txt","r");
+    char* cipher_str = (char*) malloc((no_of_characters+1) * sizeof(char));
+    encoder(plaintext_r, cipher_str);
     FILE* ciphertext = fopen("ciphertext.txt", "w");
-    fprintf(ciphertext, "%s", cipher_string);
-
-    free(cipher_string);
-    fclose(plaintext);
+    fprintf(ciphertext, "%s", cipher_str);
+    free(cipher_str);
+    fclose(plaintext_r);
     fclose(ciphertext);
-
 }
